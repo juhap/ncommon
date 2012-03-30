0e255bafe49e6dfb7eaee3017fffa510e5ba4fcf
Juha Palomaki <juhap@iki.fi>
2012-03-30 15:44:00 +0300

    Password encryption and verification added

--- tests/userstore-mongo.js
+++ tests/userstore-mongo.js
@@ -19,11 +19,12 @@ exports.tearDown = function(callback) {
 	});	
 }
 
-
 exports["Create and find user by email"] = function(test) {	
+	test.expect(2);
 	us.registerUser({
 		email: "testi@example.org",
-		passwordClear: "salasana"
+		passwordClear: "salasana",
+		name: "Testaaja"
 	}, function(error, user) {
 		test.ok(!error, "user registered");
 		us.findUserByEmail("testi@example.org", function(error, result) {		
@@ -35,3 +36,32 @@ exports["Create and find user by email"] = function(test) {
 	});
 };
 
+exports["Check password verification with incorrect password"] = function(test) {
+	test.expect(3);
+	us.registerUser({
+		email: "testuser1@example.org",
+		passwordClear: "passw0rd"
+	}, function(error, user) {	
+		test.ok(!error, "user registered");
+		us.checkPassword("testuser1@example.org", "wrongpassword", function(error, result) {
+			test.ok(!error);
+			test.ok(!result, "wrong password was used");			
+			test.done();
+		});		
+	});
+}
+
+exports["Check password verification with correct password"] = function(test) {
+	test.expect(3);
+	us.registerUser({
+		email: "testuser2@example.org",
+		passwordClear: "passw0rd"
+	}, function(error, user) {	
+		test.ok(!error, "user registered");
+		us.checkPassword("testuser2@example.org", "passw0rd", function(error, result) {
+			test.ok(!error, "error detected: " + error);
+			test.ok(result, "correct password");			
+			test.done();
+		});		
+	});
+}
